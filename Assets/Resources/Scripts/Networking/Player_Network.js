7037d4861845b28fb7eac8f30ef408e72f79cdb1
CG <heartlandergames@gmail.com>
2016-03-31 13:36:50 -0500

    Added Networking Components and Player Prefab
    Added Networking Componenets and a Player Prefab that work together.

---
+++ Assets/Resources/Scripts/Networking/Player_Network.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+import UnityEngine.Networking;
+
+public class Player_Network extends NetworkBehaviour{
+
+   
+    var FPSCamera : Camera;
+    
+    var audioListener : AudioListener;
+
+    var fpc : UnityStandardAssets.Characters.FirstPerson.FirstPersonController;
+
+function Start () {
+    if(isLocalPlayer)
+    {
+        GameObject.Find("Scene Camera").SetActive(false);
+        GetComponent(CharacterController).enabled = true;
+        fpc.enabled = true;
+        FPSCamera.enabled = true;
+        audioListener.enabled = true;
+}
+}
+
+function Update () {
+
+}
+}
